4c7d83ddcb20a5acd7681fa0ccd44313119448c7
Oluwadebo <ogunweoluwadebo@gmail.com>
2025-11-07 00:26:32 +0100

    baseurl checked

--- src/component/endpoint.js
+++ src/component/endpoint.js
@@ -1,6 +1,6 @@
 // src/endpoint.js
 let baseUrl;
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV == 'production') {
   baseUrl = 'https://washify-backend.onrender.com/api';
 } else {
   baseUrl = 'http://localhost:5000/api'; // no trailing slash
